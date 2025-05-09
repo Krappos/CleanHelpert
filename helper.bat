4bac28a4433000dca5cab04690aa703fa77201f8
Krapposko <108179984+Krappos@users.noreply.github.com>
2025-05-09 22:57:30 +0200

    formated code

--- helper.bat
+++ helper.bat
@@ -1,57 +1,65 @@
 @echo off
-
-set user=%USERNAME%
-::akopa
-::echo %user% 
-
-::zelená farba
 color 02
- 
-echo 1. turn off visual efects 
 
-::otvorenie efektov
+:: Nastavenie používateľa a disku
+set user=%USERNAME%
+set disc=%systemdrive%
+
+:: Vitaj správa
+echo Hello %user%, this app is created for you
+
+:: Krok 0: Skontroluj výkon
+echo 0. Firstly, check your performance
+start taskmgr
+echo If everything is alright then -> 
+pause
+cls
+
+:: Krok 1: Vypni vizuálne efekty
+echo 1. Turn off visual effects
+:: Otvorenie okna pre vizuálne efekty
 SystemPropertiesPerformance.exe
-pause 
+pause
+cls
 
-cls 
-
-echo 2. opening disc optimizer 
-
-::optimalizácia disku
+:: Krok 2: Otvorenie optimalizácie disku
+echo 2. Opening disc optimizer
 dfrgui.exe
 pause
+cls
 
-cls 
-
-echo 3. trash remover 
-
-::vyprázdnenie koša 
-
-::PowerShell -NoProfile -Command "Clear-RecycleBin -Force -Confirm:$false"
-
-echo trash removed 
-pause 
-
-cls 
-
-echo 4.transparency mode off 
-
-start ms-settings:personalization-colors
-
+:: Krok 3: Vyprázdniť kôš
+echo 3. Trash remover
+:: Otvorí kôš
+explorer shell:RecycleBinFolder
 pause
-
 cls
 
-echo 5. Download MalwareBytes 
+:: Krok 4: Vypni režim transparentnosti
+echo 4. Transparency mode off
+:: Otvorenie okna pre vypnutie transparentnosti
+start ms-settings:personalization-colors
+pause
+cls
 
+:: Krok 5: Stiahni MalwareBytes
+echo 5. Download MalwareBytes
+:: Automatické stiahnutie MalwareBytes do Downloads
 cd C:\Users\%user%\Downloads
-curl -O  https://data-cdn.mbamupdates.com/web/mb5-setup-consumer/MBSetup.exe
-cd .. 
-
-echo downloaded .... 
-
-pause 
-
+curl -O https://data-cdn.mbamupdates.com/web/mb5-setup-consumer/MBSetup.exe
+cd ..
+echo Downloaded...
+pause
 cls
 
+:: Krok 6: Vypnutie GameMode
+echo 6. GameMode off
+start ms-settings:gaming-gamemode
+pause
+cls
 
+:: Krok 7: Nainštalované aplikácie
+echo 7. Installed apps
+start ms-settings:appsfeatures
+pause
+cls
