e486e1e7b38bacac4cb34273f75482fd8df2a324
Krapposko <108179984+Krappos@users.noreply.github.com>
2025-05-09 22:25:20 +0200

    edits

--- helper.bat
+++ helper.bat
@@ -1,9 +1,57 @@
 @echo off
 
+set user=%USERNAME%
+::akopa
+::echo %user% 
+
+::zelená farba
 color 02
-echo pracujem na vypnuti efektov
+ 
+echo 1. turn off visual efects 
 
 ::otvorenie efektov
 SystemPropertiesPerformance.exe
+pause 
+
+cls 
+
+echo 2. opening disc optimizer 
+
+::optimalizácia disku
+dfrgui.exe
+pause
+
+cls 
+
+echo 3. trash remover 
+
+::vyprázdnenie koša 
+
+::PowerShell -NoProfile -Command "Clear-RecycleBin -Force -Confirm:$false"
+
+echo trash removed 
+pause 
+
+cls 
+
+echo 4.transparency mode off 
+
+start ms-settings:personalization-colors
+
+pause
+
+cls
+
+echo 5. Download MalwareBytes 
+
+cd C:\Users\akopa\Downloads
+curl -O  https://data-cdn.mbamupdates.com/web/mb5-setup-consumer/MBSetup.exe
+cd .. 
+
+echo downloaded .... 
+
+pause 
+
+cls
 
 
